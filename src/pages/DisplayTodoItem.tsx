6f82755905b2ee6857381573dfaa21d9c3c9161f
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-05-21 11:52:55 +0530

    refactor: update the logic for finding a todo

--- src/pages/DisplayTodoItem.tsx
+++ src/pages/DisplayTodoItem.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router";
 import { TodoContext } from "../store/TodoContext";
 import type { Todo } from "../types";
 
+import ActionButtonWrapper from "../components/ActionButtonWrapper";
+
 type params = {
   id: string;
 };
@@ -27,64 +29,48 @@ function DisplayTodoItem() {
       },
     });
   }
+  const todo = todos.todos.find((todoItem) => todoItem.id === params.id);
+  if (!todo) return;
 
   return (
     <div>
-      {todos.todos.map((todo, index) => {
-        if (todo.id === params.id) {
-          return (
-            <div
-              key={todo.id}
-              className="p-4 border border-gray-300 rounded-md shadow-sm bg-gray-50"
-            >
-              {isEditing ? (
-                <input
-                  className="border-2"
-                  type="text"
-                  ref={editRef}
-                  onBlur={() => handleInputBlur(todo)}
-                />
-              ) : (
-                <p className="font-medium text-gray-700 flex items-center justify-between">
-                  #{index + 1}: {todo.task}
-                </p>
-              )}
-              <p className="text-sm text-gray-500">
-                Created at: {todo.createdAt}
-              </p>
-              <p
-                className={`text-sm font-semibold ${
-                  todo.isDone ? "text-green-600" : "text-yellow-600"
-                }`}
-              >
-                Status: {todo.isDone ? "Completed" : "Pending"}
-              </p>
-              <div className="flex gap-2 mt-1 ml-1">
-                <button
-                  className="px-3 py-1 text-sm bg-green-500 text-white rounded hover:bg-green-600"
-                  onClick={() =>
-                    dispatch({
-                      type: "MARK_AS_DONE",
-                      payload: { id: todo.id },
-                    })
-                  }
-                >
-                  Mark as Done
-                </button>
-                <button
-                  className="px-3 py-1 text-sm bg-green-500 text-white rounded hover:bg-green-600"
-                  onClick={() => {
-                    setIsEditing(true);
-                    editRef.current?.focus();
-                  }}
-                >
-                  Edit
-                </button>
-              </div>
-            </div>
-          );
-        }
-      })}
+      <div
+        key={todo.id}
+        className="p-4 border border-gray-300 rounded-md shadow-sm bg-gray-50"
+      >
+        {isEditing ? (
+          <input
+            className="border-2"
+            type="text"
+            ref={editRef}
+            onBlur={() => handleInputBlur(todo)}
+          />
+        ) : (
+          <p className="font-medium text-gray-700 flex items-center justify-between">
+            {todo.task}
+          </p>
+        )}
+        <p className="text-sm text-gray-500">Created at: {todo.createdAt}</p>
+        <p
+          className={`text-sm font-semibold ${
+            todo.isDone ? "text-green-600" : "text-yellow-600"
+          }`}
+        >
+          Status: {todo.isDone ? "Completed" : "Pending"}
+        </p>
+        <ActionButtonWrapper
+          editFn={() => {
+            setIsEditing(true);
+            editRef.current?.focus();
+          }}
+          markAsDoneFn={() =>
+            dispatch({
+              type: "MARK_AS_DONE",
+              payload: { id: todo.id },
+            })
+          }
+        />
+      </div>
     </div>
   );
 }
